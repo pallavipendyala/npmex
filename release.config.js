ea5ca6095a52c487c02ff390bfdf729a4cfe8473
pallavi.Pendyala <pallavi.Pendyala>
2022-10-26 07:26:37 +0000

    feat: somee feature

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branches: "master",
+    repositoryUrl: "https://github.com/pallavipendyala/npmex",
+    plugins: [
+      "@semantic-release/commit-analyzer",
+      "@semantic-release/release-notes-generator",
+      "@semantic-release/github"
+    ]
+};
